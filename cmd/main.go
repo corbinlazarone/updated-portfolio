11c20d9d1c8177664297e4443933b69dbba7d814
corbinlazarone <corbinlazarone1@gmail.com>
2025-10-15 23:00:47 -0500

    feat: go server is running

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
 func main() {
+
+	port := flag.String("port", ":4000", "port the server runs on")
+
+	srv := &http.Server{
+		Addr: *port,
+	}
+
+	log.Printf("Listening on port %s", *port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
